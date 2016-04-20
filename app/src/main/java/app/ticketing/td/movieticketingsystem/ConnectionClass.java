5ca145b79b630c891c6da05eb34923ece9743a6f
Cosmin Leferman <cosminleferman@gmail.com>
2016-04-20 23:35:52 +0300

    Refactored

--- app/src/main/java/app/ticketing/td/movieticketingsystem/ConnectionClass.java
+++ app/src/main/java/app/ticketing/td/movieticketingsystem/ConnectionClass.java
@@ -7,13 +7,24 @@ import android.util.Log;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by cosmi on 4/17/2016.
  */
 public class ConnectionClass {
-    private static final String connectionString = "jdbc:jtds:sqlserver://movieticketing.database.windows.net:1433/MovieTicketingDB;user=cosmin.leferman@movieticketing;password=L3monjuice;";
+    private static final String connectionString = "jdbc:jtds:sqlserver://movieticketing.database.windows.net:1433/MovieTicketingDB;user=cosmin.leferman@movieticketing;password=Laptari3cola;";
 
-    public Connection Connect() {
+    public static Statement GetStatement() {
+        try {
+            return GetConnection().createStatement();
+        } catch (SQLException e) {
+            Log.d("ERROR", e.getMessage());
+        }
+        return null;
+    }
+
+    private static Connection GetConnection() {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         Connection conn = null;
