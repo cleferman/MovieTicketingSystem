9b119ba506f7f35b010cf5b69f0d7d94d72bc2e7
Cosmin Leferman <cosminleferman@gmail.com>
2016-05-05 23:25:54 +0300

    Cleaned up the code in MainActivity.java, removed all unnecessary functions. Wrapped private members and intent KEYS in regions. Models now implement Parcelable interface so they can be passed between activities as whole objects.

--- app/src/main/java/app/ticketing/td/movieticketingsystem/models/Cinema.java
+++ app/src/main/java/app/ticketing/td/movieticketingsystem/models/Cinema.java
@@ -1,8 +1,32 @@
 package app.ticketing.td.movieticketingsystem.models;
 
-public class Cinema {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Cinema implements Parcelable {
     private int ID;
     private String Name;
+
+    public Cinema() {
+
+    }
+
+    protected Cinema(Parcel in) {
+        ID = in.readInt();
+        Name = in.readString();
+    }
+
+    public static final Creator<Cinema> CREATOR = new Creator<Cinema>() {
+        @Override
+        public Cinema createFromParcel(Parcel in) {
+            return new Cinema(in);
+        }
+
+        @Override
+        public Cinema[] newArray(int size) {
+            return new Cinema[size];
+        }
+    };
 
     public void setID(int ID) {
         this.ID = ID;
@@ -21,7 +45,18 @@ public class Cinema {
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return Name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(ID);
+        parcel.writeString(Name);
     }
 }
